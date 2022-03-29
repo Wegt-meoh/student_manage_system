c30e47b6820fd66a8741afde79b92fedce5c703c
lasting <lastingcoder@qq.com>
2022-03-29 10:37:06 +0800

    first commit

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { message } from 'antd'
+import './index.css'
+
+export default function Login() {
+
+    const [ActicityState, setActicityState] = useState('login')
+
+    function changeActivityState() {
+        if (ActicityState === 'login') {
+            setActicityState('register')
+        } else {
+            setActicityState('login')
+        }
+        message.success("activity state change")
+    }
+
+    return (
+        <div>            
+            <div className='App-login-board'>
+                <div className='App-login-title' onClick={changeActivityState}>
+                    {ActicityState === 'login' ? 'login' : 'register'}
+                </div>
+                <div className='App-login-content'>
+                    <div className='App-login-input'>
+                        <span>EMAIL</span>
+                        <input type="text" placeholder='example@email.com' />
+                    </div>
+                    <div className='App-login-input'>
+                        <span>PASSWORD</span>
+                        <input type="text" placeholder='enter password here' />
+                    </div>
+                    <button className='App-login-button'>{ActicityState === 'login' ? 'login' : 'register'}</button>
+                </div>
+            </div>
+        </div>
+    )
+}
